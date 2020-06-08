3fea574e84e30045393184e09cb3f8563209c8e6
ithomas91 <ithomas.nemeth@gmail.com>
2020-06-08 18:14:04 +0200

    Initial Commit

---
+++ UI_Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UI_Practice
+//
+//  Created by Németh Tamás on 2020. 06. 08..
+//  Copyright © 2020. Németh Tamás. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
